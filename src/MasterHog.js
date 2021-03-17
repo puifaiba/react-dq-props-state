c323e1c2290d389db93f1fc3a119ea4a0a783180
puifaiba <puifai.ba@gmail.com>
2021-03-17 10:58:02 -0400

    dq

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
-import Master from './assets/master-hog.png'
-import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import React, {Component} from "react"
+import Master from "./assets/master-hog.png"
+import BabyHog from "./BabyHog"
+import offspring from "./db.js"
 
 export default class MasterHog extends Component {
-
   constructor() {
     super()
     this.state = {
@@ -12,41 +11,55 @@ export default class MasterHog extends Component {
     }
   }
 
-
   changeEyeColor = (e) => {
     this.setState({
-      eyeColor: e.target.value
+      eyeColor: e.target.value,
     })
   }
 
+  generateBabyHogs = (offspring) => {
+    return offspring.map((babyHog) => (
+      <BabyHog
+        id={babyHog.id}
+        name={babyHog.name}
+        hobby={babyHog.hobby}
+        eyeColor={this.state.eyeColor}
+      ></BabyHog>
+    ))
+  }
 
   render() {
     return (
       <div>
-
-        <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor} />
+        <input
+          type="radio"
+          name="eyeColor"
+          value="blue"
+          onChange={this.changeEyeColor}
+        />
         Blue<br></br>
-        <input type="radio" name="eyeColor" value="sun" onChange={this.changeEyeColor} />
+        <input
+          type="radio"
+          name="eyeColor"
+          value="sun"
+          onChange={this.changeEyeColor}
+        />
         Sun<br></br>
-        <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
+        <input
+          type="radio"
+          name="eyeColor"
+          value="glowing"
+          onChange={this.changeEyeColor}
+        />
         Glowing<br></br>
-      
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
-        <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
-        </ul>
-
+        <ul className="hoglist">{this.generateBabyHogs(offspring)}</ul>
       </div>
     )
   }
-
 }
